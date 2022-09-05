9b49dfa78dc265101a3b9b0c2347c860cc7d3e8f
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-09-05 10:55:05 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int toplam=0;
+        int sayi;
+        while(true) {
+            System.out.println("Lütfen Bir Sayı Giriniz :");
+            sayi = scan.nextInt();
+
+            if(sayi < 0) {
+                System.out.println("Program Bitti Negatif Sayı Girdiniz!");
+                System.out.println("Girilen Tek Sayılar Toplamı :" + toplam);
+                break;
+            }
+
+            if(sayi % 2 == 1) {
+                toplam +=sayi;
+            }
+        }
+
     }
 }
